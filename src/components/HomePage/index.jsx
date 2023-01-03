1e4637789bc18224e29282d7b86f45c7ea283f42
felipebravo <felipe_bravo@hotmail.com>
2023-01-03 14:24:04 -0300

    feat: add new style landing page

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,17 +1,22 @@
-import illustration from "../../assets/img/illustration.svg";
+import { Player } from "@lottiefiles/react-lottie-player";
 import "./style.css";
 
 const HomePage = ({ setIsLoggedIn }) => {
   return (
     <div className="HomePage">
       <div className="HomePage-description">
-        <h1>Your Bank</h1>
-        <p>Centralize o controle das suas finanças</p>
-        <span>de forma rápida e segura</span>
+        <h1>The Bank</h1>
+        <p>Centralize o controle das suas finanças de forma rápida e segura</p>
         <button onClick={() => setIsLoggedIn(true)}>Iniciar</button>
       </div>
       <div className="HomePage-img">
-        <img src={illustration} alt="Imagem ilustrativa" />
+        <Player
+          autoplay={true}
+          loop={true}
+          controls={false}
+          src="https://assets3.lottiefiles.com/packages/lf20_stularjd.json"
+          style={{ height: "400px", width: "90%" }}
+        ></Player>
       </div>
     </div>
   );
